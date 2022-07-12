dc4343ae2e972dcbb76ba299eba7858be80bbfbe
omkarpatne6 <omkarpatne6@gmail.com>
2022-07-12 19:44:20 +0530

    init

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import { useState, useEffect } from 'react'
 
 export async function getStaticProps(context) {
 
