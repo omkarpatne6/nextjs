5f3421a4bca748e08c8af22bae34a8d91c46d1aa
omkarpatne6 <omkarpatne6@gmail.com>
2022-07-12 20:42:39 +0530

    init

--- pages/api/fetch.js
+++ pages/api/fetch.js
@@ -1,6 +1,28 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connect from "../../util/dbconnect";
+import Mydata from "../../models/Next";
 
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
-  }
-  
+connect()
+
+export default async function handler(req, res) {
+
+    const { method } = req
+
+    if (method == "GET") {
+        try {
+            const find = await Mydata.find({});
+            res.status(200).send(find)
+        } catch (error) {
+            res.status(400).send(error)
+        }
+    } else if (method == "POST") {
+        try {
+            const send = await new Mydata(req.body);
+
+            await send.save();
+
+            res.status(200).send("Data submitted successfully");
+        } catch (error) {
+            res.status(400).send(`${e.name} : ${e.message}`)
+        }
+    }
+};
