6af215e8252db7d59c1108e34060fa0acd526c47
omkarpatne6 <omkarpatne6@gmail.com>
2022-07-12 21:03:17 +0530

    init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,28 +1,5 @@
-import connect from "../../util/dbconnect";
-import Mydata from "../../models/Next";
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-connect()
-
-export default async function handler(req, res) {
-
-    const { method } = req
-
-    if (method == "GET") {
-        try {
-            const find = await Mydata.find({});
-            res.status(200).send(find)
-        } catch (error) {
-            res.status(400).send(error)
-        }
-    } else if (method == "POST") {
-        try {
-            const send = await new Mydata(req.body);
-
-            await send.save();
-
-            res.status(200).send("Data submitted successfully");
-        } catch (error) {
-            res.status(400).send(`${e.name} : ${e.message}`)
-        }
-    }
-};
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
+}
